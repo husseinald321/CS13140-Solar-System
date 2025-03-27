7070739fd63e6dd5bed423e2e8c7fe67e56fddc9
husseinald321 <hald087@uottawa.ca>
2025-03-28 01:03:06 +0500

    rotation of earth along sun and moon along with earth

--- src/script.js
+++ src/script.js
@@ -58,8 +58,23 @@ window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+
+// initialize the clock
+const clock = new THREE.Clock();
 // render loop
 const renderloop = () => {
+  const elapsedTime = clock.getElapsedTime();
+  //add animation here
+  earth.rotation.y += 0.01;
+
+  earth.position.x = Math.sin(elapsedTime) * 10
+  earth.position.z = Math.cos(elapsedTime) * 10
+
+  moon.rotation.x =Math.sin(elapsedTime) * 2
+  moon.rotation.z =Math.cos(elapsedTime) * 2
+
+
+
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderloop);
